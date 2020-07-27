0ea0102f3663cef0933ba8d6ee0abe5aca7a5fbd
zhu0809@aliyun.com <zhu0809@aliyun.com>
2020-07-27 08:49:20 +0800

    test/cloud/CloudConfigCenter3344.java

--- cloud-config-center-3344/src/main/java/com/test/cloud/CloudConfigCenter3344.java
+++ cloud-config-center-3344/src/main/java/com/test/cloud/CloudConfigCenter3344.java
@@ -1,5 +1,8 @@
 package com.test.cloud;
 
+
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.IdcardUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -11,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class CloudConfigCenter3344 {
 
     public static void main(String[] args) {
+        IdUtil.fastSimpleUUID();
         SpringApplication.run(CloudConfigCenter3344.class, args);
     }
 }
